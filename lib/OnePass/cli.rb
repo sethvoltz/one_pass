9b11dd814dac7b06c2f20b3c589f697948964bf3
Seth Voltz <seth.voltz@relateiq.com>
2016-03-28 19:15:53 -0700

    Add mutually exclusive arguments to show command

--- lib/OnePass/cli.rb
+++ lib/OnePass/cli.rb
@@ -6,6 +6,8 @@ require 'OnePass/application'
 module OnePass
   # OnePass CLI
   class CLI < Thor
+    SHOW_OPTIONS = %w( all username password url uuid title )
+
     desc 'login', 'Save a 1Password vault and verify password'
     option :vault, aliases: '-v', type: :string, banner: 'Specify a vault path'
     def login
@@ -19,24 +21,23 @@ module OnePass
 
     desc 'show [type] {NAME|UUID}', 'Get a single item from your vault, use only one type'
     option :clip, aliases: '-c', type: :boolean, banner: 'Copy value to clipboard instead of stdout'
-    option :all, type: :boolean
-    option :username, type: :boolean
-    option :password, type: :boolean
-    option :url, type: :boolean
-    option :uuid, type: :boolean
-    option :title, type: :boolean
+    SHOW_OPTIONS.each do |switch|
+      option switch.to_sym, type: :boolean
+    end
     def show(name)
       # Check for multiple mutex args
-      # if options[:list] && options[:file]
-      #   puts "Use only one, --list(-l) or --file(-f)"
-      #   exit(0)
-      # end
-      type = 'password'
+      type = SHOW_OPTIONS.each_with_object(Hash.new) do |k, hash|
+        hash[k] = options[k] if options.has_key?(k)
+      end
+      if type.length > 1
+        puts "Use only one of #{SHOW_OPTIONS.collect { |switch| '--' + switch }.join ', '}"
+        exit 1
+      end
 
-      # Check if name looks like a UUID
+      # TODO: Check if name looks like a UUID
       # otherwise, search for title by substring, return first
       app = OnePass::Application.new
-      app.show name, type
+      app.show name, type.keys.first.to_sym
     end
 
     desc 'search QUERY', 'Perform fuzzy search for items in your vault, shows title and username'
